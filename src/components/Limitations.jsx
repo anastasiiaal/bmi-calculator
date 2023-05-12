35eb5dd645eb3d45e51dacc90ad52dfcb11d119a
Anastasiia <anastasia.alexeenko@gmail.com>
2023-05-12 10:33:40 +0200

    ✨ last component responsive css

--- src/components/Limitations.jsx
+++ src/components/Limitations.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Limitations({limits}) {
     const limitElements = limits.map(limit => {
         return (
-            <div className="limitation" key={limit.id}>
+            <div className="limitation w-[356px] xl:w-[48%] lg:w-[47%] md:w-[100%]" key={limit.id}>
                 <div className="flex items-center mb-5 ">
                     <img src={`./src/assets/icons/${limit.img}`} alt={limit.heading} />
                     <h3 className="ml-4">{limit.heading}</h3>
@@ -15,9 +15,19 @@ export default function Limitations({limits}) {
     return (
         <section className="limitations">
             <div className="container">
-                <h2>Limitations of BMI</h2>
-                <p className="text">Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial to use.</p>
-                <div className="limitations-wrapper flex justify-between flex-wrap gap-8">
+                <div className="
+                    limitations-wrapper flex justify-between flex-wrap gap-8 
+                    [&>*:nth-child(2)]:mr-24 
+                    [&>*:nth-child(3)]:ml-[390px] 
+                    [&>*:nth-child(5)]:ml-[200px] 
+                    [&>*:nth-child(6)]:mr-[190px]
+                    xl:[&>*:nth-child(n)]:ml-0 xl:[&>*:nth-child(n)]:mr-0
+                    xl:justify-center
+                ">
+                    <div className="text-wrapper xl:text-center xl:mb-6">
+                        <h2 className="mb-8">Limitations of BMI</h2>
+                        <p className="text w-[530px] xl:w-[100%]">Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial to use.</p>
+                    </div>
                     {limitElements}
                 </div>
             </div>
