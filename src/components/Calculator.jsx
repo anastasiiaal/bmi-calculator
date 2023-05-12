6854b7cc83e66fbe950d670901eb20d2c9353037
Anastasiia <anastasia.alexeenko@gmail.com>
2023-05-12 16:24:12 +0200

    🔢 calc functionality improved
    - differentiates metric vs. imperial
    - conditional template rendering depending on BMIresult (*sth* vs. undefined)

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -21,21 +21,66 @@ export default function Calculator() {
     function handleChange(event) {
         const {name, value} = event.target
         setUserData(prevData => {
-            return {
-                ...prevData,
-                [name]: value
+            if (name == "units") {
+                return {
+                    [name]: value,
+                    height: "",
+                    weight: ""
+                }
+            } else {
+                return {
+                    ...prevData,
+                    [name]: value
+                }
             }
         })
     }
 
-    function calculateBMI() {
-        if (userData.height && userData.weight) {
-            console.log(userData.weight / ((userData.height/100)*(userData.height/100)));
-        }
+    function calc (w, h) {
+        // w = weight, h = height         
+        return (w/(h*h)).toFixed(2)
     }
 
-    console.log(userData);
-    calculateBMI()
+    let BMIresult;
+    (function calculateBMI() {
+        if (userData.height && userData.weight && (
+            // got to fix input check differently...
+            (userData.units == "metric" && userData.weight.length >= 2) 
+            ||
+            (userData.units == "imperial" && userData.weight.length >= 3)
+        )) {
+            if (userData.units == "metric") {
+                const thisHeight = userData.height / 100
+                BMIresult = calc(userData.weight, thisHeight)
+            } else {
+                const thisHeight = userData.height * 12
+                const thisWeight = userData.weight * 703
+                BMIresult = calc(thisWeight, thisHeight)
+            }
+        }
+    })()
+
+    const starterTemplate = (
+        <div className="calculator__result">
+            <h3 className="mb-6">Welcome!</h3>
+            <p className="result__text">Enter your height and weight and you'll see your BMI result here</p>
+        </div>
+    )
+
+    let resultTemplate;
+    if (BMIresult === undefined) {
+        resultTemplate = starterTemplate
+    } else {
+        resultTemplate = (
+            <div className="calculator__result flex items-center justify-between sm:flex-col sm:items-start sm:gap-8">
+                <div>
+                    <p className="result__text">Your BMI is...</p>
+                    <h2 className="mt-6">{BMIresult}</h2>
+                </div>
+                <p className="result__text w-[250px] pr-8 sm:w-[100%] sm:pr-0">Your BMI suggests that you have a healthy weight. Your ideal weight is between <span>55.6kgs - 60.3kgs</span></p>
+            </div>
+        )
+    }
 
     return (
         <div className="calculator w-[565px] lg:w-[100%]">
@@ -78,6 +123,8 @@ export default function Calculator() {
                             placeholder="0"
                             onChange={handleChange}
                             value={userData.height}
+                            // min={1}
+                            // max={100}
                         />
                         <h3 className="measure-unit">{height}</h3>
                     </div>
@@ -90,22 +137,14 @@ export default function Calculator() {
                             placeholder="0"
                             onChange={handleChange}
                             value={userData.weight}
+                            // min={1}
+                            // max={100}
                         />
                         <h3 className="measure-unit">{weight}</h3>
                     </div>
                 </div>
             </form>
-            <div className="calculator__result">
-                <h3 className="mb-6">Welcome!</h3>
-                <p className="result__text">Enter your height and weight and you'll see your BMI result here</p>
-            </div>
-            {/* <div className="calculator__result flex items-center justify-between sm:flex-col sm:items-start sm:gap-8">
-                <div>
-                    <p className="result__text">Your BMI is...</p>
-                    <h2 className="mt-6">23.4</h2>
-                </div>
-                <p className="result__text w-[250px] pr-8 sm:w-[100%] sm:pr-0">Your BMI suggests that you have a healthy weight. Your ideal weight is between <span>55.6kgs - 60.3kgs</span></p>
-            </div> */}
+            {resultTemplate}
         </div>
     )
 }
